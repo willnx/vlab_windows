0c2b23dc9932dd7b6d187ccb766597e3c1b1de85
willnx <willnx84@gmail.com>
2018-12-07 09:20:23 -0800

    deprecated v1 auth tokens

--- vlab_windows_api/lib/views/windows.py
+++ vlab_windows_api/lib/views/windows.py
@@ -57,7 +57,7 @@ class WindowsView(TaskView):
                     }
 
 
-    @requires(verify=const.VLAB_VERIFY_TOKEN, version=(1,2))
+    @requires(verify=const.VLAB_VERIFY_TOKEN, version=2)
     @describe(post=POST_SCHEMA, delete=DELETE_SCHEMA, get=GET_SCHEMA)
     def get(self, *args, **kwargs):
         """Display the Windows instances you own"""
@@ -70,7 +70,7 @@ class WindowsView(TaskView):
         resp.headers.add('Link', '<{0}{1}/task/{2}>; rel=status'.format(const.VLAB_URL, self.route_base, task.id))
         return resp
 
-    @requires(verify=const.VLAB_VERIFY_TOKEN, version=(1,2))
+    @requires(verify=const.VLAB_VERIFY_TOKEN, version=2)
     @validate_input(schema=POST_SCHEMA)
     def post(self, *args, **kwargs):
         """Create a Windows"""
@@ -87,7 +87,7 @@ class WindowsView(TaskView):
         resp.headers.add('Link', '<{0}{1}/task/{2}>; rel=status'.format(const.VLAB_URL, self.route_base, task.id))
         return resp
 
-    @requires(verify=const.VLAB_VERIFY_TOKEN, version=(1,2))
+    @requires(verify=const.VLAB_VERIFY_TOKEN, version=2)
     @validate_input(schema=DELETE_SCHEMA)
     def delete(self, *args, **kwargs):
         """Destroy a Windows"""
@@ -102,7 +102,7 @@ class WindowsView(TaskView):
         return resp
 
     @route('/image', methods=["GET"])
-    @requires(verify=const.VLAB_VERIFY_TOKEN, version=(1,2))
+    @requires(verify=const.VLAB_VERIFY_TOKEN, version=2)
     @describe(get=IMAGES_SCHEMA)
     def image(self, *args, **kwargs):
         """Show available versions of Windows that can be deployed"""
