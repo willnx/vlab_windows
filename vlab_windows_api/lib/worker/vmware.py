7439940c86c6d9a540b58791929cf2b51c877c52
willnx <willnx84@gmail.com>
2019-06-24 12:39:27 -0700

    Updated to support vlab-inf-common==2019.06.24

--- vlab_windows_api/lib/worker/vmware.py
+++ vlab_windows_api/lib/worker/vmware.py
@@ -22,7 +22,7 @@ def show_windows(username):
                  password=const.INF_VCENTER_PASSWORD) as vcenter:
         folder = vcenter.get_by_name(name=username, vimtype=vim.Folder)
         for vm in folder.childEntity:
-            info = virtual_machine.get_info(vcenter, vm)
+            info = virtual_machine.get_info(vcenter, vm, username)
             if info['meta']['component'] == 'Windows':
                 windows_vms[vm.name] = info
     return windows_vms
@@ -47,7 +47,7 @@ def delete_windows(username, machine_name, logger):
         folder = vcenter.get_by_name(name=username, vimtype=vim.Folder)
         for entity in folder.childEntity:
             if entity.name == machine_name:
-                info = virtual_machine.get_info(vcenter, entity)
+                info = virtual_machine.get_info(vcenter, entity, username)
                 if info['meta']['component'] == 'Windows':
                     logger.debug('powering off VM')
                     virtual_machine.power(entity, state='off')
@@ -106,7 +106,7 @@ def create_windows(username, machine_name, image, network, logger):
                      'generation': 1,
                     }
         virtual_machine.set_meta(the_vm, meta_data)
-        info = virtual_machine.get_info(vcenter, the_vm, ensure_ip=True)
+        info = virtual_machine.get_info(vcenter, the_vm, username, ensure_ip=True)
         return {the_vm.name: info}
 
 
@@ -156,7 +156,7 @@ def update_network(username, machine_name, new_network):
         folder = vcenter.get_by_name(name=username, vimtype=vim.Folder)
         for entity in folder.childEntity:
             if entity.name == machine_name:
-                info = virtual_machine.get_info(vcenter, entity)
+                info = virtual_machine.get_info(vcenter, entity, username)
                 if info['meta']['component'] == 'Windows':
                     the_vm = entity
                     break
